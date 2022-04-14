607537fd975e77afae38dcd9cf02bd21b3d66e00
Boze-noob <bozeskoko16@gmail.com>
2022-04-14 14:43:50 +0200

    done announcement dialog refactored

--- lib/ui/pages/requests/widgets/company/done_announcement_dialog.dart
+++ lib/ui/pages/requests/widgets/company/done_announcement_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:contract_management/_all.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DoneAnnouncementDialog extends StatefulWidget {
   final AnnouncementModel announcementModel;
@@ -20,16 +21,29 @@ class _DoneAnnouncementDialogState extends State<DoneAnnouncementDialog> {
   @override
   Widget build(BuildContext context) {
     return BlocListener<WorkDiariesBloc, WorkDiariesState>(
-      listener: (context, state) {
-        if (state.status == WorkDiariesStateStatus.loaded)
+      listener: (context, state) async {
+        if (state.status == WorkDiariesStateStatus.loaded) {
+          //added cuz app gets frozen
+          await Future.delayed(Duration(seconds: 2));
           context.workDiariesBloc.add(
             WorkDiariesUpdateByIdEvent(
               announcementId: widget.announcementModel.id,
             ),
           );
+        }
       },
       child: CustomDialog(
         buttonText: 'Send',
+        onButtonPressed: () {
+          context.companyRequestsBloc.add(
+              CompanyEditAnnouncementRequestsEvent(
+                  announcementStatusType:
+                  AnnouncementStatusType.inProgress,
+                  announcementId: widget.announcementModel.id));
+          //TODO add announcement completed here
+
+
+        } ,
         message: 'Documents will be sent to admin for approve',
         child: Expanded(
           child: Padding(
@@ -82,15 +96,6 @@ class _DoneAnnouncementDialogState extends State<DoneAnnouncementDialog> {
                       size: context.textSizeM,
                       color: Colors.black,
                     ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    CustomText(
-                      text: 'Billing',
-                      size: context.textSizeL,
-                      color: Colors.black,
-                      weight: FontWeight.bold,
-                    ),
                     Padding(
                       padding: EdgeInsets.symmetric(vertical: 15),
                       child: Divider(
@@ -98,9 +103,15 @@ class _DoneAnnouncementDialogState extends State<DoneAnnouncementDialog> {
                         color: Colors.grey,
                       ),
                     ),
+                    CustomText(
+                      text: 'Billing',
+                      size: context.textSizeL,
+                      color: Colors.black,
+                      weight: FontWeight.bold,
+                    ),
                     _AdditionalCostWidget(),
                     SizedBox(
-                      height: 10,
+                      height: 5,
                     ),
                     _TotalCostWidget(
                       announcementModel: widget.announcementModel,
@@ -108,11 +119,6 @@ class _DoneAnnouncementDialogState extends State<DoneAnnouncementDialog> {
                     SizedBox(
                       height: 10,
                     ),
-                    Button(
-                      text: 'Send',
-                      shrinkWrap: true,
-                      onTap: () => null,
-                    )
                   ],
                 );
               },
@@ -146,50 +152,96 @@ class __AdditionalCostWidgetState extends State<_AdditionalCostWidget> {
         SizedBox(
           width: 5,
         ),
-        IconButton(
-          onPressed: () => showDialog(
-            context: context,
-            builder: (BuildContext context) => Dialog(
-              child: Column(
-                children: [
-                  CustomText(
-                    text: 'Additional costs details',
-                    size: context.textSizeL,
-                    textAlign: TextAlign.center,
-                    weight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  CustomText(
-                    text: context.workDiariesBloc.state.workDiaryModel!.additionalRequirements,
-                    size: context.textSizeM,
-                    color: Colors.black,
-                  )
-                ],
+        Builder(
+          builder: (parentContext) {
+            return IconButton(
+              onPressed: () => showDialog(
+                  context: parentContext,
+                  builder: (BuildContext context) {
+                    if (parentContext.workDiariesBloc.state.workDiaryModel == null)
+                      return Loader(
+                        width: 100,
+                        height: 100,
+                        color: active,
+                      );
+                    return Dialog(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomText(
+                              text: 'Additional costs details',
+                              size: context.textSizeL,
+                              textAlign: TextAlign.center,
+                              weight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            CustomText(
+                              text: parentContext.workDiariesBloc.state.workDiaryModel!.additionalRequirements.value,
+                              size: context.textSizeM,
+                              color: Colors.black,
+                            ),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Button(
+                              text: 'Ok',
+                              shrinkWrap: true,
+                              color: active,
+                              onTap: () => Get.back(),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+              icon: Icon(
+                Icons.info_outline,
               ),
-            ),
-          ),
-          icon: Icon(
-            Icons.info_outline,
-          ),
+            );
+          },
         ),
         IconButton(
           onPressed: () => showDialog(
               context: context,
               builder: (BuildContext context) => Dialog(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          initialValue: additionalCost,
-                          onChanged: (text) => setState(() {
-                            additionalCost = text;
-                          }),
-                          style: TextFormFieldStyle.inputFieldTextStyle(),
-                          decoration: TextFormFieldStyle.inputDecoration('Additional cost'),
-                        )
-                      ],
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomText(
+                            text: 'Edit price',
+                            textAlign: TextAlign.center,
+                            size: context.textSizeM,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            initialValue: additionalCost,
+                            onChanged: (text) => setState(() {
+                              additionalCost = text;
+                            }),
+                            style: TextFormFieldStyle.inputFieldTextStyle(),
+                            decoration: TextFormFieldStyle.inputDecoration('Additional cost'),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Button(
+                            text: 'Ok',
+                            shrinkWrap: true,
+                            color: active,
+                            onTap: () => Get.back(),
+                          )
+                        ],
+                      ),
                     ),
                   )),
           icon: Icon(
@@ -232,17 +284,37 @@ class __TotalCostWidgetState extends State<_TotalCostWidget> {
           onPressed: () => showDialog(
               context: context,
               builder: (BuildContext context) => Dialog(
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          initialValue: totalPrice,
-                          onChanged: (text) => setState(() {
-                            totalPrice = text;
-                          }),
-                          style: TextFormFieldStyle.inputFieldTextStyle(),
-                          decoration: TextFormFieldStyle.inputDecoration('Total price'),
-                        )
-                      ],
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomText(
+                            text: 'Edit price',
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            initialValue: totalPrice,
+                            onChanged: (text) => setState(() {
+                              totalPrice = text;
+                            }),
+                            style: TextFormFieldStyle.inputFieldTextStyle(),
+                            decoration: TextFormFieldStyle.inputDecoration('Total price'),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Button(
+                            text: 'Ok',
+                            shrinkWrap: true,
+                            color: active,
+                            onTap: () => Get.back(),
+                          )
+                        ],
+                      ),
                     ),
                   )),
           icon: Icon(
