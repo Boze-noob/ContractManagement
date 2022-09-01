dc69a5a80faf189d0d526985702e283fbaeb06ec
Boze-noob <bozeskoko16@gmail.com>
2022-09-01 10:25:41 +0200

    refactored code

--- lib/ui/pages/requests/widgets/admin/view_order_dialog.dart
+++ lib/ui/pages/requests/widgets/admin/view_order_dialog.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 // ignore: must_be_immutable
 class ViewOrderDialog extends StatelessWidget {
   OrderModel orderModel;
+
   ViewOrderDialog({Key? key, required this.orderModel}) : super(key: key);
 
   @override
@@ -16,115 +17,133 @@ class ViewOrderDialog extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              CustomText(
-                text: 'Sender name: ${orderModel.senderName.value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Receiver name: ${orderModel.receiverName.value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Order location: ${orderModel.orderLocation.value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Payment type: ${orderModel.paymentType.translate().value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Create date time: ${orderModel.createdDateTime.formatDDMMYY().value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Completion date time: ${orderModel.completionDateTime.formatDDMMYY().value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text:
-                    'Sent date time: ${orderModel.sentDateTime != null ? orderModel.sentDateTime!.formatDDMMYY().toString() : 'Not defined yet'}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Divider(
-                color: Colors.black12.withOpacity(0.6),
-              ),
-              CustomText(text: 'Contract items:'),
-              SizedBox(
-                height: 5,
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: orderModel.contractItems.length,
-                itemBuilder: (context, i) {
-                  return ListTile(
-                    title: Text(' - ' + orderModel.contractItems[i].translate()),
-                  );
-                },
-              ),
-              Divider(
-                color: Colors.black12.withOpacity(0.6),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Employer name: ${orderModel.employerName.value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Order status type: ${orderModel.orderStatusType.translate().value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Admin request type: ${orderModel.adminRequestType.translate().value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              CustomText(
-                text: 'Client name: ${orderModel.clientName.value}',
-                weight: FontWeight.normal,
-                color: Colors.black,
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CustomText(
+                          text: 'Client name: ${orderModel.senderName.value}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Receiver name: ${orderModel.receiverName.value}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Order location: ${orderModel.orderLocation.value}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Payment type: ${orderModel.paymentType.translate().value}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Price: ${orderModel.price} KM',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Created date time: ${orderModel.createdDateTime.formatDDMMYY().value}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Completion date time: ${orderModel.completionDateTime.formatDDMMYY().value}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text:
+                              'Sent date time: ${orderModel.sentDateTime != null ? orderModel.sentDateTime!.formatDDMMYY().toString() : 'Not defined yet'}',
+                          weight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        CustomText(
+                          text: 'Employer name: ${orderModel.employerName.value}',
+                          weight: FontWeight.bold,
+                          color: Colors.black,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Order status type: ${orderModel.orderStatusType.translate().value}',
+                          weight: FontWeight.bold,
+                          color: Colors.black,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Admin request type: ${orderModel.adminRequestType.translate().value}',
+                          weight: FontWeight.bold,
+                          color: Colors.black,
+                          size: 18,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        CustomText(
+                          text: 'Contract items:',
+                          weight: FontWeight.bold,
+                          color: Colors.black,
+                          size: 18,
+                        ),
+                        ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: orderModel.contractItems.length,
+                          itemBuilder: (context, i) {
+                            return ListTile(
+                              title: CustomText(
+                                text: ' - ' + orderModel.contractItems[i].translate(),
+                                size: 18,
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
